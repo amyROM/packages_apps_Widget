abd081400b455d43e76fd7e235777972c0d5aa57
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-01-21 16:11:19 +0530

    WaveWidget: Cleanup!
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- src/me/waveproject/widget/BaseApplication.kt
+++
@@ -1,5 +0,0 @@
-package me.waveproject.widget
-
-import android.app.Application
-
-class BaseApplication : Application() 
--- src/me/waveproject/widget/WaveWidgetProvider.kt
+++ src/me/waveproject/widget/WaveWidgetProvider.kt
@@ -12,7 +12,11 @@ import android.widget.RemoteViews
  * Implementation of App Widget functionality.
  */
 class WaveWidgetProvider : AppWidgetProvider() {
-    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -26,28 +30,30 @@ class WaveWidgetProvider : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+}
 
-    companion object {
-        fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager,
-                            appWidgetId: Int) {
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.wave_widget)
 
-            // Construct the RemoteViews object
-            val views = RemoteViews(context.packageName, R.layout.wave_widget)
+    val clockPackage = "com.android.deskclock"
+    val clockClass = "com.android.deskclock.DeskClock"
+    val clockIntent = Intent(Intent.ACTION_MAIN)
+        .addCategory(Intent.CATEGORY_LAUNCHER)
+        .setComponent(
+            ComponentName(
+                clockPackage,
+                clockClass
+            )
+        )
+    val pendingIntent = PendingIntent.getActivity(context, 0, clockIntent, 0)
+    // Open clock app on click
+    views.setOnClickPendingIntent(R.id.wave_widget, pendingIntent)
 
-            val clockPackage = "com.android.deskclock"
-            val clockClass = "com.android.deskclock.DeskClock"
-            val clockIntent = Intent(Intent.ACTION_MAIN)
-                    .addCategory(Intent.CATEGORY_LAUNCHER)
-                    .setComponent(ComponentName(
-                            clockPackage,
-                            clockClass)
-                    )
-            val pendingIntent = PendingIntent.getActivity(context, 0, clockIntent, 0)
-            // Open clock app on click
-            views.setOnClickPendingIntent(R.id.wave_widget, pendingIntent)
-
-            // Instruct the widget manager to update the widget
-            appWidgetManager.updateAppWidget(appWidgetId, views)
-        }
-    }
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
 }
